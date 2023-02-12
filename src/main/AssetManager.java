aa228f450951f975cb432959f97b48ddf147ff41
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2023-02-12 09:47:28 -0300

    Update AssetManager.java

--- src/main/AssetManager.java
+++ src/main/AssetManager.java
@@ -4,45 +4,46 @@ import object.OBJ_Boots;
 import object.OBJ_Chest;
 import object.OBJ_Door;
 import object.OBJ_Key;
+import object.SuperObject;
 
 public class AssetManager {
 	GamePanel gp;
+	int indexCounter = 0;
 	
 	public AssetManager(GamePanel gp) {
 		this.gp = gp;
 	}
 	
+	void set(String type, int tileX, int tileY) {
+		SuperObject obj = null;
+		
+		if(type == "key") 		obj = new OBJ_Key(gp);
+		if(type == "door") 		obj = new OBJ_Door(gp);
+		if(type == "chest") 	obj = new OBJ_Chest(gp);
+		if(type == "boots")		obj = new OBJ_Boots(gp);
+
+		
+		obj.worldX = tileX * gp.tileSize;
+		obj.worldY = tileY * gp.tileSize;
+		
+		gp.obj[indexCounter] = obj;
+		
+		indexCounter++;
+	}
+	
 	public void setObject() {
-		gp.obj[0] = new OBJ_Key(gp);
-		gp.obj[0].worldX = 23 * gp.tileSize;
-		gp.obj[0].worldY = 7 * gp.tileSize;
+		set("key", 30, 37);
+		set("key", 23, 40);
+		set("key", 8, 41);
 		
-		gp.obj[1] = new OBJ_Key(gp);
-		gp.obj[1].worldX = 23 * gp.tileSize;
-		gp.obj[1].worldY = 40 * gp.tileSize;
+		set("door", 10, 12);
+		set("door", 12, 23);
+		set("door", 30, 40);
 		
-		gp.obj[2] = new OBJ_Key(gp);
-		gp.obj[2].worldX = 38 * gp.tileSize;
-		gp.obj[2].worldY = 8 * gp.tileSize;
 		
-		gp.obj[3] = new OBJ_Door(gp);
-		gp.obj[3].worldX = 10 * gp.tileSize;
-		gp.obj[3].worldY = 12 * gp.tileSize;
 		
-		gp.obj[4] = new OBJ_Door(gp);
-		gp.obj[4].worldX = 8 * gp.tileSize;
-		gp.obj[4].worldY = 28 * gp.tileSize;
+		set("boots", 37, 42);
 		
-		gp.obj[5] = new OBJ_Door(gp);
-		gp.obj[5].worldX = 12 * gp.tileSize;
-		gp.obj[5].worldY = 23 * gp.tileSize;
-		
-		gp.obj[6] = new OBJ_Chest(gp);
-		gp.obj[6].worldX = 10 * gp.tileSize;
-		gp.obj[6].worldY = 8 * gp.tileSize;
-		
-		gp.obj[7] = new OBJ_Boots(gp);
-		gp.obj[7].worldX = 37 * gp.tileSize;
-		gp.obj[7].worldY = 42 * gp.tileSize;
+		set("chest", 10, 8);
 	}
 }
