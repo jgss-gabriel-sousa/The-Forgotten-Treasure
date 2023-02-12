80605b9d1fedbde6b01e0d51e15ea3870efd0630
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2023-02-12 14:32:24 -0300

    Update Player.java

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -4,14 +4,10 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.InputHandler;
 import main.Sound;
-import main.UtilityTool;
 
 public class Player extends Entity {
 	GamePanel gp;
