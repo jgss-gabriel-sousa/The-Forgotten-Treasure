7ef2c913376e1ada6f881a118b65fe8acf2bf10a
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2023-02-12 09:50:15 -0300

    UI Improvement

--- src/main/UI.java
+++ src/main/UI.java
@@ -10,8 +10,8 @@ import object.OBJ_Key;
 
 public class UI {
 	GamePanel gp;
-	Font arial_40, arial_80B;
-	BufferedImage keyImage;
+	Font arial_28, arial_80B;
+	BufferedImage keyImage, swordImage;
 	public boolean messageOn = false;
 	public String message = "";
 	int messageCounter = 0;
@@ -24,7 +24,7 @@ public class UI {
 	public UI(GamePanel gp) {
 		this.gp = gp;
 		
-		arial_40 = new Font("Arial", Font.PLAIN, 40);
+		arial_28 = new Font("Arial", Font.PLAIN, 28);
 		arial_80B = new Font("Arial", Font.BOLD, 80);
 		OBJ_Key key = new OBJ_Key(gp);
 		keyImage = key.image;
@@ -39,17 +39,19 @@ public class UI {
 		if(gp.debug) {
 			g2.setColor(Color.white);
 			g2.setFont(g2.getFont().deriveFont(10F));
-			g2.drawString("Time: "+dFormat.format(playTime), 5, 10);
-			g2.drawString("Player Speed: "+gp.player.speed, 5, 20);
-			g2.drawString("Player Direction: "+gp.player.direction, 5, 30);
-			g2.drawString("Player Key Count: "+gp.player.keyCount, 5, 40);
-			g2.drawString("Draw Time: "+dFormat3.format(gp.drawTime/10000000)+"ms", 5, 50);
-			int xPos = gp.player.worldX+(gp.tileSize/2);
-			int yPos = gp.player.worldY+(gp.tileSize/2);
+			int i = 1;
+			g2.drawString("Time: "+dFormat.format(playTime), 5, i++*10);
+			g2.drawString("Player Speed: "+gp.player.speed, 5, i++*10);
+			g2.drawString("Player Direction: "+gp.player.direction, 5, i++*10);
+			g2.drawString("Player Sprite: "+gp.player.spriteNum, 5, i++*10);
+			g2.drawString("Player Key Count: "+gp.player.keyCount, 5, i++*10);
+			g2.drawString("Draw Time: "+dFormat3.format(gp.drawTime/10000000)+"ms", 5, i++*10);
+			int xPos = gp.player.worldX + (gp.tileSize/2);
+			int yPos = gp.player.worldY + (gp.tileSize/2);
 			int xTile = xPos / gp.tileSize;
 			int yTile = yPos / gp.tileSize;
-			g2.drawString("Position: "+xPos+", "+yPos, 5, 70);
-			g2.drawString("Tile: "+xTile+", "+yTile, 5, 60);
+			g2.drawString("Position: "+xPos+", "+yPos, 5, i++*10);
+			g2.drawString("Tile: "+xTile+", "+yTile, 5, i++*10);
 		}
 	}
 	
@@ -60,13 +62,21 @@ public class UI {
 		int y;
 		
 		if(gameFinished) {
-			g2.setFont(arial_40);
-			g2.setColor(Color.white);
-			
+			g2.setFont(g2.getFont().deriveFont(30F));
+
 			text = "You found the forgotten treasure!";
 			textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+			int textHeigth = (int)g2.getFontMetrics().getStringBounds(text, g2).getHeight();
 			x = gp.screenWidth/2 - textLength/2;
-			y = gp.screenHeight/2 + (gp.tileSize*2);
+			y = gp.screenHeight - (gp.tileSize/2);
+
+			g2.setColor(Color.darkGray);
+			g2.fillRect(x-5,y-textHeigth,textLength+10,textHeigth+10);
+			
+			g2.setColor(Color.black);
+			g2.drawRect(x-5,y-textHeigth,textLength+10,textHeigth+10);
+
+			g2.setColor(Color.white);
 			g2.drawString(text, x, y);
 
 			g2.setFont(arial_80B);
@@ -75,10 +85,9 @@ public class UI {
 			text = "Congratulations!";
 			textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
 			x = gp.screenWidth/2 - textLength/2;
-			y = gp.screenHeight/2 - (gp.tileSize*3);
+			y = gp.tileSize*2;
 			g2.drawString(text, x, y);
 			
-
 			g2.setFont(g2.getFont().deriveFont(30F));
 
 			String timeText;
@@ -90,7 +99,7 @@ public class UI {
 			text = "Your Time is: "+timeText;
 			textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
 			x = gp.screenWidth/2 - textLength/2;
-			y = gp.screenHeight - (gp.tileSize);
+			y = (gp.tileSize*3)+10;
 			g2.drawString(text, x, y);
 
 			debugUI(g2);
@@ -102,22 +111,41 @@ public class UI {
 				debugUI(g2);
 			}
 			else {
-				g2.setFont(arial_40);
+				g2.setColor(Color.darkGray);
+				g2.fillRect((gp.tileSize/2)-10, (gp.tileSize/2)-10, gp.tileSize*2-5, gp.tileSize*2-10);
+				
+				g2.setColor(Color.black);
+				g2.drawRect((gp.tileSize/2)-10, (gp.tileSize/2)-10, gp.tileSize*2-5, gp.tileSize*2-10);
+
+				g2.setFont(arial_28);
 				g2.setColor(Color.white);
-				g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
-				g2.drawString("x "+gp.player.keyCount, 74, 65);
+				
+				int iconSize = (int)(gp.tileSize/1.5);
+				
+				g2.drawImage(keyImage, (gp.tileSize/2)-5, (gp.tileSize/2)-5, iconSize, iconSize, null);
+				g2.drawString("x "+gp.player.keyCount, 55, 50);
+				
 			}
 			
 			playTime += (double)1/60;
 
 			
 			if(messageOn) {
+				
 				g2.setFont(g2.getFont().deriveFont(30F));
 				
 				textLength = (int)g2.getFontMetrics().getStringBounds(message, g2).getWidth();
+				int textHeigth = (int)g2.getFontMetrics().getStringBounds(message, g2).getHeight();
 				x = gp.screenWidth/2 - textLength/2;
-				y = gp.screenHeight - (gp.tileSize);
+				y = gp.screenHeight - (gp.tileSize/2);
+
+				g2.setColor(Color.darkGray);
+				g2.fillRect(x-5,y-textHeigth,textLength+10,textHeigth+10);
 				
+				g2.setColor(Color.black);
+				g2.drawRect(x-5,y-textHeigth,textLength+10,textHeigth+10);
+
+				g2.setColor(Color.white);
 				g2.drawString(message, x, y);
 				
 				messageCounter++;
